f862a99c5d0004f1cfa82c0a93b984252075bd06
Lucy <lucynasowaa@gmail.com>
2024-10-10 13:52:04 +0200

    simple server

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@ const messagesSection = document.querySelector('#messages-section');
 const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-message-form');
 const userNameInput = document.querySelector("#username");
-const messageContentInput = document.querySelector("#text-input");
+const messageContentInput = document.querySelector(".add-messages-form .text-input");
 
 const loginBtn = document.querySelector('.welcome-form .btn');
 const submitBtn = document.querySelector('.add-messages-form .btn');
@@ -25,8 +25,40 @@ const loginHandler = function(event) {
 
 const sendMessage = function(event) {
     event.preventDefault();
-    console.log('sendMessage')
+    let message = messageContentInput.value;
+
+    if (!message) {
+        alert('please type message');
+    } else {
+       addMessage(userName, messageContentInput.value); 
+       message = ''
+    }
 }
 
 loginBtn.addEventListener('click', loginHandler);
-submitBtn.addEventListener('click', sendMessage)
+submitBtn.addEventListener('click', sendMessage);
+
+function addMessage(author, content) {
+const message = document.createElement('li');
+message.classList.add('message');
+message.classList.add('message--received');
+
+if(author === userName) {
+    message.classList.add('message--self');
+}
+const header = document.createElement('h3');
+header.classList.add('message__author');
+if(author === userName){
+    header.innerHTML = author;  
+} else {
+    header.innerHTML = 'You';
+}
+
+const div = document.createElement('div');
+div.classList.add('message__content');
+div.innerHTML = content;
+message.appendChild(header);
+message.appendChild(div);
+
+messagesList.appendChild(message);
+}
